9724a83983b47454dd800cb3195f0b7dc9092a6a
himazmhmd <himazmhmd@gmail.com>
2020-09-18 04:35:51 +0530

    created csc Example java file

---
+++ cscExamples.java
@@ -0,0 +1,13 @@
+public class cscExamples{
+	int questionNo;
+	String category;
+	String text;
+	
+	public void setQuestionNo(){
+	}
+	public void setcategory(){
+	}
+	public void setcategoryext(){
+	}
+	
+}
