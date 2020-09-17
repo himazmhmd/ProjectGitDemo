d8bb4027140d77e46e2b2b3e83b8c75e25306ac4
himazmhmd <himazmhmd@gmail.com>
2020-09-18 04:50:38 +0530

    added get methods to csc Example java file

---
+++ cscExamples.java
@@ -0,0 +1,19 @@
+public class cscExamples{
+	int questionNo;
+	String category;
+	String text;
+	
+	public void setQuestionNo(){
+	}
+	public void setcategory(){
+	}
+	public void setcategoryext(){
+	}
+	public int setQuestionNo(){
+	}
+	public String setcategory(){
+	}
+	public String setText(){
+	}
+	
+}
